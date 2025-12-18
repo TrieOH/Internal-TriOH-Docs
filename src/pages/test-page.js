2ac6a8acecfc9132b17532f4f46db75e3870d264
MintzyG <ericbraga2000@gmail.com>
2025-12-18 14:00:48 -0300

    test

---
+++ src/pages/test-page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+
+export default function MyReactPage() {
+  return (
+    <Layout>
+      <h1>My React page</h1>
+      <p>I m testing Docusaurus YAYAYAYAYAY</p>
+    </Layout>
+  );
+}
